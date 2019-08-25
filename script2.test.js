156e9d88b2b5a47dcadf6f37b9ed552cc2b9ca67
xiuw <sylvi.xw@gmail.com>
2019-08-25 00:53:03 -0400

    test with mock api call

--- script2.test.js
+++ script2.test.js
@@ -9,9 +9,26 @@ it('Calling swapi api(AsyncAwait)', ()=>{ // when running asynchrous test, alway
 })
 
 it('Calling swapi api(Promise)', ()=>{
-	expect.assertions(2)
+	expect.assertions(2) //2 assertions
 	return starwarapi.getPeoplePromise(fetch).then(data=>{
 		expect(data.count).toEqual(87);
-		expect(data.results.length).toBeGreaterThan(5);
+		expect(data.results.length).toBeGreaterThan(9);
+	})
+})
+
+it('Return count and results', ()=>{
+	const mockFetch = jest.fn()
+	.mockReturnValue(Promise.resolve({//mock the data
+		json: ()=>Promise.resolve({//always assuming the promise is resolve
+			count:87,
+			results:[0,1,2,3,4,5,6,7,8,9,10]
+		})
+	}))  //Mock the api call 
+	expect.assertions(4)
+	return starwarapi.getPeoplePromise(mockFetch).then(data => {
+		expect(mockFetch.mock.calls.length).toBe(1) //number of this to be call is 1
+		expect(mockFetch).toBeCalledWith('https://swapi.co/api/people')
+		expect(data.count).toEqual(87)
+		expect(data.results.length).toBeGreaterThan(9)
 	})
 })
